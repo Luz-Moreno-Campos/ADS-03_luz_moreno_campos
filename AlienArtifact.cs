419989321d12dad5bf9f52a24fd9867305ad06b5
Luz Moreno Campos <luzdirection@gmail.com>
2026-07-01 10:36:29 -0500

    Created AlienArtifact class and added properties

---
+++ AlienArtifact.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace ADS_03_luz_moreno_campos
+{
+    public class AlienArtifact
+    {
+        //Wrote it in camelCase because it is a private const, which I will use to validate description in description setter.
+        private const int maxDescriptionLength = 500;
+
+        public string EncodedName { get; private set; }
+        public string DecodedName { get; private set; }
+        public string Planet { get; private set; }
+
+        //Used string for DiscoveryDate because the dates are not valid, but fictional galactic cycles.
+        public string DiscoveryDate { get; private set; }
+        public string StorageLocation { get; private set; }
+
+        // I chose to validate the description in the setter instead of the constructor for stronger encapsulation.
+        private string _description;
+        public string Description
+        {
+
+            get { return _description; }
+            private set
+            {
+                if (value.Length > maxDescriptionLength)
+                {
+                    throw new ArgumentOutOfRangeException("Description exceeds maximum length.");
+                }
+
+                _description = value;
+            }
+        }
+    }
+}
