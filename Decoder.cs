c58e5d2baaeb39709779e07ec4df036765ffbe51
Luz Moreno Campos <luzdirection@gmail.com>
2026-07-01 15:30:57 -0500

    divided responsibility into methods and created first method

--- Decoder.cs
+++ Decoder.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace ADS_03_luz_moreno_campos
 {
-    internal class Decoder
+    public static class Decoder
     {
+        private static readonly char[] Original = { 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y', 'Z' };
+
+        private static readonly char[] Mapped = { 'H','Z','A','U','Y','E','K','G','O','T','I','R','J','V','W','N','M','F','Q','S','D','B','X','L','C', 'P' };
+
+
+        //Divided name decoding in small methods  for more clarity and easier debugging:
+        //1.GetOriginalIndex method:  gets the index of the letter in the original array.
+        //2. DecodeLetter method: uses the index to map chars and moves levels until it reaches the base case.
+        //3. ReverseMapping method (DecodeLetter base case)
+        //4.DecodeName method: decodes  full name using DecodeLetter method
+
+        private static int GetOriginalIndex(char codeChar)
+
+        {
+            for (int i = 0; i < Original.Length; i++)
+            {
+                if (Original[i] == codeChar)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+       
+
+
+
+
+
+
+
+
     }
 }
