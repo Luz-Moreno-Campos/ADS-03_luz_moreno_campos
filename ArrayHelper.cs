6a063d8c4771eda4864767a4984ebd69f75d2a98
Luz Moreno Campos <luzdirection@gmail.com>
2026-07-01 19:58:12 -0500

    created BinarySearchByDecodedName method

--- ArrayHelper.cs
+++ ArrayHelper.cs
@@ -2,7 +2,7 @@
 
 namespace ADS_03_luz_moreno_campos
 {
-    public static class ArrayHelper
+    public static class ArrayHelper 
     {
         //Method to resize array when new artifacts are added
         public static AlienArtifact[] ResizeArray(AlienArtifact[] original)
@@ -46,6 +46,51 @@ namespace ADS_03_luz_moreno_campos
             {
                 array[i] = array[i - 1];
             }
+        }
+
+
+        //Method to search for duplicates
+        public static int BinarySearchByDecodedName(AlienArtifact[] array, string decodedName)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(
+                    paramName: null,
+                    message: "Array cannot be null.");
+            }
+
+            if (decodedName == null)
+            {
+                throw new ArgumentNullException(
+                    paramName: null,
+                    message: "Decoded Name cannot be null.");
+            }
+
+            int left = 0;
+            int right = array.Length - 1;
+
+            while (left <= right)
+            {
+                int mid= left + (right - left) / 2;
+                string midDecodedName = array[mid].DecodedName;
+
+                int comparison = midDecodedName.CompareTo(decodedName);
+
+                if (comparison == 0)
+                {
+                    return mid; 
+                }
+                else if (comparison < 0)
+                {
+                    left = mid + 1; 
+                }
+                else
+                {
+                    right = mid - 1; 
+                }
+            }
+
+            return -1; 
         }
 
 
