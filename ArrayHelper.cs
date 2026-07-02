9ca2de91b7fcd630562c2317824faa5eaeb28d39
Luz Moreno Campos <luzdirection@gmail.com>
2026-07-01 19:14:09 -0500

    created ArrayHelper class and added method for array resizing

---
+++ ArrayHelper.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace ADS_03_luz_moreno_campos
+{
+    public static class ArrayHelper
+    {
+        //Method to resize array when new artifacts are added
+        public static AlienArtifact[] ResizeArray(AlienArtifact[] original)
+        {
+            if (original == null)
+            {
+                throw new ArgumentNullException(
+                    paramName: null,
+                    message: "Array cannot be null.");
+            }
+
+            AlienArtifact[] bigger = new AlienArtifact[original.Length * 2];
+
+            for (int i = 0; i < original.Length; i++)
+            {
+                bigger[i] = original[i];
+            }
+
+            return bigger;
+        }
+
+
+    }
+}
